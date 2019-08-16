a6096514f304dd4b9e0358e5d7f3fa9e2942a48e
eevan7a9 <e7a9@protonmail.com>
2019-08-17 01:55:35 +0900

    created new component Todolists

---
+++ src/components/TodoLists.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="card">
+    <div class="card-body p-4">
+      <div class="form-group">
+        <label for></label>
+        <input type="text" name id class="form-control" placeholder aria-describedby="helpId" />
+        <small id="helpId" class="text-muted">New Todo</small>
+      </div>
+    </div>
+    <div class="card-footer">
+      <button class="btn btn-primary">Add +</button>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "TodoLists"
+};
+</script>
+
+<style scoped>
+main {
+  text-align: center;
+}
+</style>
