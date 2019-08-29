67e54a6334185cf71d1957d396ac711c9c062d8b
eevan7a9 <e7a9@protonmail.com>
2019-08-29 22:15:10 +0900

    change base url

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 Vue.use(Vuex);
 // the base url of our api from our backend server
-axios.defaults.baseURL = 'http://localhost/Todo_backend/public/api';
+axios.defaults.baseURL = 'https://todo-laravue.herokuapp.com/api';
 
 export default new Vuex.Store({
   state: {
